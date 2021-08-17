a2060ef029f69f3cc32df5dd8e0fccc3a2dc1af8
luckyeverest <gribkovdmitryy@gmail.com>
2021-08-17 15:39:33 +0500

    start pages and img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <headers>
+        <div className='headerLeft'>
+          <img width={40} height={40} src='../img/logo.png' />
+          <div className='headerInfo'>
+            <h3 className=''>
+              Магазин кросовок
+            </h3>
+            <p className=''>Магазин лучших кросовок</p>
+          </div>
+        </div>
+        <ul className='headerRight'>
+          <li className=''>
+            <img width={18} height={18} src="img/cart.svg" alt="Корзина" />
+            <span className=''>120 rub</span>
+          </li>
+          <li className=''>
+            <img width={18} height={18} src="img/user.svg" alt="Пользователь" />
+          </li>
+        </ul>
+      </headers>
+      <div className='content'>
+        <h1>Все кросовки</h1>
+        .................
+      </div>
     </div>
   );
 }
